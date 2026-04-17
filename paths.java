d85ba53722daf1d3d24eb7851829412929ce8807
AHS CS Student <ahs-cs@ausd>
2026-04-17 14:58:08 -0700

    gg

--- paths.java
+++ paths.java
@@ -1,3 +1,21 @@
-public class paths {
-    
+public class paths{
+    private int startY, startX;
+
+    public paths(){
+        this.startY=1;
+        this.startX=1;
+        int p = pathCount(startX, startY);
+        System.out.println(p);
+    }
+  
+    //this returns the number of path between 0,0 and
+    //n,k restricted by moving north and west only
+    public int pathCount(int n, int k){
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        new paths();
+    }
 }
+
