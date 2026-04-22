5671f80cc7c186b941028837bdcf0d016cea6bee
AHS CS Student <ahs-cs@ausd>
2026-04-22 14:17:05 -0700

    asdffghj

--- Towers.java
+++ Towers.java
@@ -16,6 +16,7 @@ public class Towers{
 
   //YOU::you write this -- simple ArrayList move the last item of l to r
   private void moveFromLeftToRight(ArrayList<Integer> l,  ArrayList<Integer> r){
+    r.add(l.remove(l.size()-1));
 
   }
 
@@ -24,6 +25,10 @@ public class Towers{
       if(discs == 1){
         moveFromLeftToRight(l, r);
       } else{
+        solve(discs-1, l, r, m);
+        moveFromLeftToRight(l, r);  
+        solve(discs-1, m, l, r);
+       
         //recursive moment -- this three lines! and BIG HINT: two recursive calls to this method solve
         //as well as another call to moveFromLeftToRight
       }     
@@ -44,5 +49,8 @@ public class Towers{
     System.out.println("Middle: " + middleT);
     System.out.println("Right: " + rightT);
 
+  }
+  public static void main(String[] args) {
+    new Towers();
   }
 }
